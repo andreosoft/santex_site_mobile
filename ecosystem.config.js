28ba200d78541fe4994f10c30627f7c20c5fe183
Andrei <andreyzagorets@gmail.com>
2023-12-23 22:32:44 +0300

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    apps: [
-      {
-        name: 'santex_site_mibile',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-      }
-    ]
-  }
-  
-  
-  
+  apps: [
+    {
+      name: 'santex_site_mibile',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
+
+
