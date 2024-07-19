51a6d2730bdb08803be6294addf005b67523cb11
Andrei <andreyzagorets@gmail.com>
2024-07-19 17:24:29 +0300

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,21 +10,7 @@
 </template>
 
 <script>
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(97871715, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-<noscript>
-  <div>
-      <img src="https://mc.yandex.ru/watch/97871715" style="position:absolute; left:-9999px;" alt="" />
-  </div>
-  </noscript>
+export default {
+  name: 'DefaultLayout'
+}
 </script>
