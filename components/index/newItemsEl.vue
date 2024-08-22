ca400031018dfe3367d802ef88aaac1f6fba0cd0
Andrei <andreyzagorets@gmail.com>
2024-08-22 20:13:54 +0300

    commit

--- components/index/newItemsEl.vue
+++ components/index/newItemsEl.vue
@@ -1,8 +1,9 @@
 <template>
   <v-card>
     <NuxtLink :to="'/promote/' + el.id">
-      <v-img class="w-100 h-100" v-if="el.images_mobile && el.images_mobile[0]" :src="$config.baseImageURL + el.images_mobile[0]" />
-      <v-img class="w-100 h-100" v-else-if="el.images && el.images[0]" :src="$config.baseImageURL + el.images[0]" />
+      <img class="w-100 h-100" v-if="el.images_mobile && el.images_mobile[0]"
+        :src="$config.baseImageURL + el.images_mobile[0] + '?height=500'" />
+      <img class="w-100 h-100" v-else-if="el.images && el.images[0]" :src="$config.baseImageURL + el.images[0]" />
       <div class="s-cart-tite-1">
         <div class="pa-10 d-flex">
           <div class="d-inline-block mr-3" style="vertical-align: top">
